9292c08a4af50c6704a35e08c0c31aa020a669c3
Artem Bahmet <baget.masta@gmail.com>
2022-07-14 05:55:58 +0300

    add files for hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,13 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-console.log(galleryItems);
 
+const bodyRef = document.querySelector('body');
 const galleryContainer = document.querySelector('.gallery');
 const cardsMarkup = createCardsMarkup(galleryItems);
+// const forTest = document.querySelector('.basicLightbox--visible');
 
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
 galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+// galleryContainer.addEventListener('click', isModalOpen);
 
 function createCardsMarkup(cards) {
   return cards
@@ -29,4 +32,41 @@ function createCardsMarkup(cards) {
 
 function onGalleryContainerClick(evt) {
   evt.preventDefault();
+
+  const wayToImg = evt.target.dataset.source;
+
+  basicLightBoxOpen(wayToImg);
+  // addSpecialClass();
+  // removeSpecialClass();
 }
+
+function basicLightBoxOpen(wayToImg) {
+  basicLightbox
+    .create(`<img width="1400" height="900" src="${wayToImg}">`)
+    .show();
+
+  addSpecialClass();
+  // const visible = basicLightbox.visible();
+
+  // isActive(visible);
+}
+
+function addSpecialClass() {
+  bodyRef.classList.toggle('modal-open');
+}
+
+// function isModalOpen(evt) {
+//   if (bodyRef.classList.contains('modal-open')) {
+//     bodyRef.classList.remove('modal-open');
+//   }
+// }
+
+// function isActive(boolean) {
+//   console.log(boolean);
+// }
+
+// function removeSpecialClass() {
+//   if (galleryContainer.classList.contains('modal-open')) {
+//     galleryContainer.classList.remove('modal-open');
+//   }
+// }
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,5 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 console.log(galleryItems);
 
@@ -13,20 +14,15 @@ function createCardsMarkup(cards) {
   return cards
     .map(({ preview, original, description }) => {
       return `
-      <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-          <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-          />
-        </a>
-      </div>`;
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
     })
     .join('');
 }
 
 function onGalleryContainerClick(evt) {
   evt.preventDefault();
+
+  const lightbox = new SimpleLightbox('.gallery a', {});
 }
